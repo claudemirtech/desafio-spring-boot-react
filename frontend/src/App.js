224c326c7c4f70d6eaa9ac36c4bb2b819381ae46
Claudemir Pinto <claudemir@bsd.com.br>
2022-11-05 08:32:28 -0300

    Remove diretório node

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ClientList from './ClientList';
-import ClientEdit from "./ClientEdit";
+import ClientEdit from './ClientEdit';
 
 class App extends Component {
   render() {
